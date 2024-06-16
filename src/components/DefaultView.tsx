4006afbce8c28bfb18f5dffb43c9a559d2f062e3
iam-agf <agf0710@gmail.com>
2024-06-16 00:53:11 -0600

    Default view removed not needed logic

--- src/components/DefaultView.tsx
+++ src/components/DefaultView.tsx
@@ -1,10 +1,5 @@
-import { FlexboxGrid } from "rsuite";
-import FlexboxGridItem from "rsuite/esm/FlexboxGrid/FlexboxGridItem";
-import Sidebar from "./Sidebar/Sidebar";
+import SidebarView from "./Sidebar/Sidebar";
 import Connect from "../services/adena/connectButton";
-import { useContext, useEffect, useState } from "react";
-import ProviderContext from "../context/ProviderContext";
-import { parseResponse } from "../pieces/supportFuns";
 import 'rsuite/FlexboxGrid/styles/index.css';
 
 interface Content {
@@ -12,44 +7,21 @@ interface Content {
 }
 
 const DefaultView: React.FC<Content> = ({ component: Component }) => {
-    const [active, setActive] = useState('home');
-
-    const [owner, setOwner] = useState<string | null>(null);
-    const { provider } = useContext(ProviderContext);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                if (provider) {
-                    var creator = await provider.evaluateExpression('gno.land/r/dev/shikenrepository', 'GetCreator()');
-                    creator = parseResponse(creator)
-                    setOwner(creator);
-                } else {
-                    const creator = ""
-                    setOwner(creator);
-                }
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-
-        fetchData();
-    }, [provider]);
     return (
         <>
-            <FlexboxGrid>
-                <FlexboxGridItem colspan={6}>
-                    <Sidebar />
-                </FlexboxGridItem>
-                <FlexboxGridItem colspan={12}>
+            <div className="grid bg-secondary-600">
+                <div className="col-1">
+                    <SidebarView />
+                </div>
+                <div className="col-8">
                     <Component />
-                </FlexboxGridItem>
-                <FlexboxGrid.Item colspan={6}>
+                </div>
+                <div className="col-3">
                     <div style={{ display: "flex", flexDirection: "column" }}>
                         <Connect />
                     </div>
-                </FlexboxGrid.Item>
-            </FlexboxGrid>
+                </div>
+            </div >
         </>
     );
 };
