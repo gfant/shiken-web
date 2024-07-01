2fd87971c38061e99675f5492bd759b7fe590a5c
iam-agf <agf0710@gmail.com>
2024-07-01 00:26:11 -0600

    Generate Hash to attempt

--- src/components/Problem/SendCodeButton.tsx
+++ src/components/Problem/SendCodeButton.tsx
@@ -1,11 +1,11 @@
-export const SendCodeButton = () => {
+export const SendButton = ({content}: {content:string}) => {
     return (
         <div className="grid">
             <div className='col-offset-5 col-2'>
                 <div className='m-1 p-1 border-round hover:border-1 bg-primary hover:bg-white hover:text-purple-500'>
                     <div className="flex align-items-center justify-content-center font-bold">
                         <div className="flex flex-column md:flex-row">
-                            <p>Send Solution</p>
+                            <p>{content}</p>
                         </div>
                     </div>
                 </div>
@@ -14,4 +14,4 @@ export const SendCodeButton = () => {
     );
 };
 
-export default SendCodeButton;
+export default SendButton;
--- src/components/SendSolution.tsx
+++ src/components/SendSolution.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Config from './../config';
 import axios from "axios";
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Card } from "primereact/card";
-import SendCodeButton from "./Problem/SendCodeButton";
 import { Toast } from "primereact/toast";
+import { AdenaService } from "../services/adena/adena";
+import { EMessageType } from "../services/adena/adena.types";
+import AccountContext from "../context/AccountContext";
+import SendButton from "./Problem/SendCodeButton";
+import { MsgSend } from "@gnolang/gno-js-client";
 
 interface Output {
     output: string,
@@ -14,8 +18,10 @@ interface Output {
 
 
 const SendSolution = ({ id }: { id: string }) => {
-    const [code, setCode] = useState<string>('')
     const [count, setCount] = useState(0);
+    const { address } = useContext(AccountContext);
+    const [code, setCode] = useState<string>('')
+    const [hash, setHash] = useState<string>('')
     const [response, setResponse] = useState<Output>({} as Output)
     const toast = useRef<Toast>(null);
 
@@ -35,6 +41,37 @@ const SendSolution = ({ id }: { id: string }) => {
         }
     }, [response, count])
 
+    const SendCrypto = async () => {
+        if (address === "" || address === null) {
+            // Wallet not connected
+            showToast({ severity: "error", summary: "Connect your wallet", detail: "" })
+            return;
+        }
+
+        try {
+            const sendMsg: MsgSend = {
+                from_address: address,
+                to_address: "g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5",
+                amount: "100000 ugnot",
+            };
+
+            const getTx = await AdenaService.sendTransaction(
+                [
+                    {
+                        type: EMessageType.MSG_SEND,
+                        value: sendMsg
+                    }
+                ],
+                5000000
+            );
+            setHash(getTx.hash)
+        } catch (e) {
+            console.error(e);
+
+        }
+
+    };
+
     const RunCode = () => {
         const input = code.replace(/ /g, '\t').replace(/\n/g, '\n');
         axios
@@ -44,7 +81,7 @@ const SendSolution = ({ id }: { id: string }) => {
             }, {
                 headers: {
                     "Access-Control-Allow-Headers": "Content-Type",
-                    'Access-Control-Allow-Origin':"*",
+                    'Access-Control-Allow-Origin': "*",
                     'Content-Type': 'application/json',
                     "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT"
                 }
@@ -65,7 +102,12 @@ const SendSolution = ({ id }: { id: string }) => {
                         <InputTextarea autoResize id="code" value={code} onChange={e => setCode(e.target.value)} cols={60} rows={12} className="text-white justify-center col-9" />
                     </div>
                 </div>
-                <div onClick={() => { RunCode() }}><SendCodeButton /></div>
+                {hash === "" ? <div onClick={() => { SendCrypto() }}><SendButton content={"Generate hash"} /></div> : <div onClick={() => { RunCode() }}><SendButton content={"Send Solution"} /></div>}
+                <div className="col-8 col-offset-3">
+                    {hash !== "" ? <div>
+                        <p>Hash for attempt: <b>{hash}</b></p>
+                    </div> : <></>}
+                </div>
                 <br />
             </Card>
         </>
