670e698d267a6d3fdd5726635a1f02b7148d78ca
gfant <61362029+gfant@users.noreply.github.com>
2024-07-26 23:47:01 -0600

    Connect button disappears when connected

--- src/components/DefaultView.tsx
+++ src/components/DefaultView.tsx
@@ -1,11 +1,18 @@
 import SidebarView from "./Sidebar/Sidebar";
 import Connect from "../services/adena/connectButton";
+import { useContext, useEffect } from "react";
+import AccountContext from "../context/AccountContext";
 
 interface Content {
     component: React.ComponentType;
 }
 
 const DefaultView: React.FC<Content> = ({ component: Component }) => {
+    const { address } = useContext(AccountContext);
+
+    useEffect(() => {
+        console.log(address)
+    }, [])
     return (
         <>
             <div className="grid bg-secondary-600">
@@ -17,7 +24,12 @@ const DefaultView: React.FC<Content> = ({ component: Component }) => {
                 </div>
                 <div className="col-2">
                     <div style={{ display: "flex", flexDirection: "column" }}>
-                        <Connect />
+                        {address ? (
+                            <div>Current Address: {address}</div>
+                        ) : (<div>
+                            <Connect/>
+                        </div>
+                        )}
                     </div>
                 </div>
             </div >
--- src/services/adena/connectButton.tsx
+++ src/services/adena/connectButton.tsx
@@ -7,53 +7,37 @@ import AccountContext from '../../context/AccountContext';
 
 const Connect: FC<IConnectProps> = () => {
     const { setChainID, setAddress } = useContext(AccountContext);
-    const [addr, setAddr] = useState<string>("");
-    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const handleWalletConnect = async () => {
-        setIsLoading(true);
         try {
-            // Attempt to establish a connection
             await AdenaService.establishConnection("");
-
-            // Get the account info
+            await AdenaService.switchNetwork(Config.CHAIN_ID);
             const info: IAccountInfo = await AdenaService.getAccountInfo();
 
-            // Make sure the network is valid
-            await AdenaService.switchNetwork(Config.CHAIN_ID);
-
-            setAddr(info.address);
-            // Update the account context
+            
             setAddress(info.address);
             setChainID(Config.CHAIN_ID);
         } catch (e) {
             console.error(e);
         }
-
-        setIsLoading(false);
     };
 
     return (
         <div>
-            {isLoading ?
-                <>
-                    <div className="col-12" onClick={handleWalletConnect}>
-                        <div className='bg-primary text-primary border-round hover:bg-white hover:text-purple-500'>
-                            <div className="flex flex-column md:flex-row">
-                                <div className="flex align-items-center p-2 justify-content-center font-bold border-round">
-                                    <i className="pi pi-wallet" />
-                                </div>
-                                <div className="flex align-items-center p-2 justify-content-center font-bold border-round">
-                                    <p>Connect</p>
-                                </div>
+            <>
+                <div className="col-12" onClick={handleWalletConnect}>
+                    <div className='bg-primary text-primary border-round hover:bg-white hover:text-purple-500'>
+                        <div className="flex flex-column md:flex-row">
+                            <div className="flex align-items-center p-2 justify-content-center font-bold border-round">
+                                <i className="pi pi-wallet" />
+                            </div>
+                            <div className="flex align-items-center p-2 justify-content-center font-bold border-round">
+                                <p>Connect</p>
                             </div>
                         </div>
                     </div>
-                </> :
-                <>
-                    Current Address: {addr ? addr : ""}
-                </>
-            }
+                </div>
+            </>
         </div>
     );
 };
