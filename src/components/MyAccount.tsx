7704fd03c7fd8bced82818e206f8792334acf3f2
gfant <61362029+gfant@users.noreply.github.com>
2024-08-17 23:45:59 -0600

    Warn wallet not connected

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -1,5 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-import DefaultView from "./DefaultView";
+import { useContext } from "react";
 import AccountContext from "../context/AccountContext";
 import ProblemsSolved from "./UserAccount/ProblemsSolved";
 
@@ -13,7 +12,7 @@ const MyAccount = () => {
     return (
         <div>
             <h1 onClick={handleCopyToClipboard} style={{ cursor: 'pointer', userSelect: 'none' }}>
-                My account ({account.address})
+                My account {account.address?.length == 40 ? <span>({account.address})</span>: <span>(Not connected)</span>}
             </h1>
             <div>
                 <ProblemsSolved/>
